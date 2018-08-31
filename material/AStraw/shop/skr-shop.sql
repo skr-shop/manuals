ee59fb0b1faf9b8f2e6b06ccda9fcc724b78066b
TIGERB <tigerbcode@gmail.com>
2018-08-31 14:58:53 +0800

    doc(AStraw): shop

--- material/AStraw/shop/skr-shop.sql
+++ material/AStraw/shop/skr-shop.sql
@@ -31,14 +31,14 @@ CREATE TABLE `shop`  (
   `intro` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '店铺简介',
   `area_id` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '店铺最下级地址id',
   `area_address` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '店铺详细地址，不包含省市区的信息',
-  `shop_note` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '店铺留言，如休息中留言',
+  `shop_notice` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '店铺留言，如休息中留言',
   `status` tinyint(2) UNSIGNED NOT NULL DEFAULT 0 COMMENT '店铺状态：默认0-新申请，10-审核通过，11-审核中，20-审核失败，30-营业中，40-休息中',
   `sort` mediumint(5) UNSIGNED NOT NULL DEFAULT 0 COMMENT '排序值',
   `last_verify_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '最后审核时间',
-  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '添加时间',
-  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+  `create_at` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '添加时间',
+  `update_at` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '店铺基本信息表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '店铺基本信息表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for shop_certificate
@@ -50,10 +50,10 @@ CREATE TABLE `shop_certificate`  (
   `identity_card` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '店主身份证信息，json格式存正反面地址',
   `other_card` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '其他证件信息，json格式存多张图片地址',
   `verify_memo` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '提交审核留言',
-  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '添加时间',
-  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+  `create_at` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '添加时间',
+  `update_at` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '店铺相关证件信息表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '店铺相关证件信息表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for shop_goods_category
@@ -66,10 +66,10 @@ CREATE TABLE `shop_goods_category`  (
   `icon_url` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '分类图标地址',
   `status` tinyint(2) UNSIGNED NOT NULL DEFAULT 0 COMMENT '有效状态：默认0-有效，1-无效',
   `sort` mediumint(5) UNSIGNED NOT NULL DEFAULT 0 COMMENT '排序值',
-  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '添加时间',
-  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+  `create_at` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '添加时间',
+  `update_at` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '店铺自定义商品分类表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '店铺自定义商品分类表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for shop_industry
@@ -81,10 +81,10 @@ CREATE TABLE `shop_industry`  (
   `intro` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '行业简介',
   `status` tinyint(2) UNSIGNED NOT NULL DEFAULT 0 COMMENT '有效状态：默认0-有效，1-无效',
   `sort` mediumint(5) UNSIGNED NOT NULL DEFAULT 0 COMMENT '排序值',
-  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '添加时间',
-  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+  `create_at` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '添加时间',
+  `update_at` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '店铺行业表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '店铺行业表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for shop_verify
@@ -98,9 +98,9 @@ CREATE TABLE `shop_verify`  (
   `operator_staffid` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '审核人后台staffid',
   `operator_staffname` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '审核人信息',
   `verify_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '最后审核时间',
-  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '添加时间',
-  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+  `create_at` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '添加时间',
+  `update_at` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '店铺审核记录表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '店铺审核记录表' ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
